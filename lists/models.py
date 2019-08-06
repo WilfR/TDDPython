d57113f685f8f9e4207eae201edf8dfee1b76aaa
Wilf Rosenbaum <Wilf.Rosenbaum@gmail.com>
2019-08-05 21:28:22 -0700

    Redirect after post, and show all items in template

--- lists/models.py
+++ lists/models.py
@@ -2,21 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class ItemList :
-    def __init__(self) :
-        self.objects = []
-    def all( self ) :
-        return self
-    def count( self ) :
-        return len( self.objects )
-    def addItem( self, item ) :
-        self.objects.append(item)
-    def __getitem__( self, i ) :
-        return self.objects[i]
+class Item(models.Model):
+    text = models.TextField(default='')
 
-class Item():
-    objects = ItemList()
-    def __init__( self ) :
-        self.text = None
-    def save( self ) :
-        Item.objects.addItem( self )
+
--- lists/tests.py
+++ lists/tests.py
@@ -15,9 +15,34 @@ class HomePageTest( TestCase ) :
 
 
     def testCanSaveAPostRequest( self ) :
+
+        ### Test initial state
+        self.assertEqual(Item.objects.count(), 0)
+
+        ### Execute the Post and test expected state (side effects of Post)
         response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertEqual(Item.objects.count(), 1)
+        newItem = Item.objects.first()
+        self.assertEqual(newItem.text, 'A new list item')
+
+    def testRedirectsAfterPost( self ) :
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+
+    def testSavesOnlyWhenNecessary( self ) :
+        self.assertEqual(Item.objects.count(), 0)
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+
+    def testDisplayAllListItems( self ) :
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+        response = self.client.get('/')
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
 
 class ItemModelTest( TestCase ) :
 
