4f207bc87012983c5e6f8b11b50207de9546185d
Wilf Rosenbaum <Wilf.Rosenbaum@gmail.com>
2019-08-13 21:56:05 -0700

    First steps to make the site prettier

--- FunctionalTests/tests.py
+++ FunctionalTests/tests.py
@@ -153,5 +153,21 @@ class NewVisitorTest( LiveServerTestCase ) :
         self.assertNotIn( 'Buy peacock feathers', pageText )
         self.assertIn( 'Get milk', pageText )
 
+    def testLayoutAndStyling( self ) :
+
+        self.browser.get( self.live_server_url )
+        self.browser.set_window_size( 1024, 768 )
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+
+        self.assertAlmostEqual( inputbox.location['x'] + inputbox.size['width']/2, 512, delta=10 )
+
+        ### start a new list and see that the input box is centered there too
+
+        self.sendTextToInputBox('Buy peacock feathers')
+        self.waitForRowInListTable('1: Buy peacock feathers')
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual( inputbox.location['x'] + inputbox.size['width']/2, 512, delta=10 )
 
 
