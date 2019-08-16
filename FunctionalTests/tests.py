76cadb0df337ffc7ea2d618b3c2a603d7c9a7a99
Wilf Rosenbaum <Wilf.Rosenbaum@gmail.com>
2019-08-15 17:33:45 -0700

    Added some bootstrap prettyfing

--- FunctionalTests/tests.py
+++ FunctionalTests/tests.py
@@ -1,4 +1,4 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
@@ -14,7 +14,7 @@ import time
 
 MAX_WAIT_TIME = 10
 
-class NewVisitorTest( LiveServerTestCase ) :
+class NewVisitorTest( StaticLiveServerTestCase ) :
 
     ###
     ###     Utilities
@@ -159,6 +159,8 @@ class NewVisitorTest( LiveServerTestCase ) :
         self.browser.set_window_size( 1024, 768 )
 
         inputbox = self.browser.find_element_by_id('id_new_item')
+
+        print("---> xloc={} width={}".format(inputbox.location['x'],inputbox.size['width']))
 
         self.assertAlmostEqual( inputbox.location['x'] + inputbox.size['width']/2, 512, delta=10 )
 
