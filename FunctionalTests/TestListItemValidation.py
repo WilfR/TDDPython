b9997d09354b99b32eed7f375c4448cbae1950e8
Wilf Rosenbaum <Wilf.Rosenbaum@gmail.com>
2019-09-07 18:14:04 -0700

    Implement duplicate item validation at model layer

--- FunctionalTests/TestListItemValidation.py
+++ FunctionalTests/TestListItemValidation.py
@@ -38,3 +38,21 @@ class ItemValidationTest( FunctionalTest ) :
         self.waitForRowInListTable('1: Buy milk')
         self.waitForRowInListTable('2: Make tea')
 
+
+
+    def testCannotAddDuplicateItems(self):
+
+        # Edith goes to the home page and starts a new list
+        self.browser.get(self.live_server_url)
+        self.sendTextToInputBox('Buy wellies')
+        self.waitForRowInListTable('1: Buy wellies')
+
+        # She accidentally tries to enter a duplicate item
+        self.sendTextToInputBox('Buy wellies')
+
+        # She sees a helpful error message
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text, "You've already got this in your list"
+            ))
+
+
--- lists/tests/testModels.py
+++ lists/tests/testModels.py
@@ -3,6 +3,23 @@ from lists.models import Item, List
 from django.core.exceptions import ValidationError
 
 
+class ItemModelTest( TestCase ) :
+
+    def testDefaultItemText( self )  :
+        item = Item()
+        self.assertEqual( item.text, '' )
+
+    def testStringRepresentation(self):
+        item = Item(text='some text')
+        self.assertEqual(str(item), 'some text')
+
+class ListModelTest( TestCase ) :
+
+    def testAbsoluteURL( self ) :
+        theList = List.objects.create()
+        self.assertEqual(theList.get_absolute_url(), f'/lists/{theList.id}/')
+
+
 class ListAndItemModelTest( TestCase ) :
 
     def testSavingAndRetrievingItems( self ) :
@@ -32,6 +49,13 @@ class ListAndItemModelTest( TestCase ) :
         self.assertEqual( "Item the second", secondSavedItem.text )
         self.assertEqual( theList, secondSavedItem.list )
 
+    def testItemIsRelatedToList( self ) :
+        theList = List.objects.create()
+        item = Item()
+        item.list = theList
+        item.save()
+        self.assertIn( item, theList.item_set.all() )
+
     def testCannotSaveEmptyListItem( self ) :
         theList = List.objects.create()
         item = Item( list=theList, text='')
@@ -39,6 +63,37 @@ class ListAndItemModelTest( TestCase ) :
             item.save()
             item.full_clean()
 
-    def testAbsoluteURL( self ) :
+    def testDuplicateItemsAreInvalid( self ) :
         theList = List.objects.create()
-        self.assertEqual(theList.get_absolute_url(), f'/lists/{theList.id}/')
+        Item.objects.create( text='Get some wibbler', list=theList )
+        with self.assertRaises( ValidationError ) :
+            item = Item( text='Get some wibbler', list=theList )
+            item.full_clean()
+
+    def testCanSaveSameItemToMultipleLists ( self ) :
+        list1 = List.objects.create()
+        list2 = List.objects.create()
+        Item.objects.create( text='Get some wibbler', list=list1 )
+        item = Item( text='Get some wibbler', list=list2 )
+        item.full_clean() # should not raise an exception
+
+    def testDuplicateItemsAreInvalidAlternativeImplementation( self ) :
+        theList = List.objects.create()
+        item1 = Item( text='Get some wibbler', list=theList )
+        item1.full_clean()
+        item1.save()
+        with self.assertRaises( ValidationError ) :
+            item2 = Item( text='Get some wibbler', list=theList )
+            item2.full_clean()
+
+    def testListOrderingIsPreservedUnderItemCreation(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='i1')
+        item2 = Item.objects.create(list=list1, text='item 2')
+        item3 = Item.objects.create(list=list1, text='3')
+        self.assertEqual(
+            list(Item.objects.all()),
+            [item1, item2, item3]
+            )
+
+
