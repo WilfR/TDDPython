c1448c2c06d522e46f7994ebed08e21e6b45cb5a
Wilf Rosenbaum <Wilf.Rosenbaum@gmail.com>
2019-09-16 13:13:19 -0700

    use environment variable for site name for allowed host

--- superlists/settings.py
+++ superlists/settings.py
@@ -32,7 +32,7 @@ if 'DJANGO_DEBUG_FALSE' in os.environ:
     DEBUG = False
     SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
     ALLOWED_HOSTS = [os.environ['SITENAME']]
-    ALLOWED_HOSTS = ['104.225.208.105','.wlrosenbaum.ca']
+    ### ALLOWED_HOSTS = ['104.225.208.105','.wlrosenbaum.ca']
 else:
     DEBUG = True
     SECRET_KEY = 'insecure-key-for-dev'
